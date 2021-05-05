2cf4f16f924d8d827a6431b833ef93397421cd6a
Ilya Sychugov <ilya.sychugov@gmail.com>
2021-05-05 18:07:30 +0300

    Fix: try 4

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -11,9 +11,9 @@ export default function PopupWithForm(props){
       <form action="/" name={props.name} className="popup__container" noValidate onSubmit={handleSubmit}>
         <button type="button" className="popup__close-button" onClick={props.onClose}></button>
         <fieldset className="popup__fieldset">
-          {props.title && <h2 className="popup__title">{props.title}</h2>}
+          {props.title && (<h2 className="popup__title">{props.title}</h2>)}
           {props.children}
-          {props.btnName && <button type="submit" className="popup__save-button">{props.btnName}</button>}
+          {props.btnName && (<button type="submit" className="popup__save-button">{props.btnName}</button>)}
         </fieldset>
       </form>
     </div>
